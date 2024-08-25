aecbedd0a271de8527ee62bde046c77377ed0817
jaswantg98 <jaswantgolla98@gmail.com>
2024-08-25 12:33:42 -0400

    Changes to GlobalExceptionHandler class

--- src/main/java/org/receipt/generator/exception/GlobalExceptionHandler.java
+++ src/main/java/org/receipt/generator/exception/GlobalExceptionHandler.java
@@ -2,10 +2,10 @@ package org.receipt.generator.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidReceiptRequestException.class)
